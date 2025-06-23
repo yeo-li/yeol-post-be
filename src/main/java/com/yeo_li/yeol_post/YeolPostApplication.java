a1028447f1e383b674088ba601d163914bdfc36b
yeo-li <parkseongyeol2110@gmail.com>
2025-06-23 10:52:05 +0900

    feat : Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/yeo_li/yeol_post/YeolPostApplication.java
@@ -0,0 +1,13 @@
+package com.yeo_li.yeol_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeolPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeolPostApplication.class, args);
+	}
+
+}
