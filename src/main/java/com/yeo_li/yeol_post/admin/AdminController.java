dee42ff678ff87fdb326f320525733ecddc691b6
yeo-li <parkseongyeol2110@gmail.com>
2025-07-14 21:11:19 +0900

    refactor: getCurrentUser() 사용자 인가 로직 추가(#13)

--- src/main/java/com/yeo_li/yeol_post/admin/AdminController.java
+++ src/main/java/com/yeo_li/yeol_post/admin/AdminController.java
@@ -1,45 +1,41 @@
 package com.yeo_li.yeol_post.admin;
 
-import jakarta.servlet.http.Cookie;
+import com.yeo_li.yeol_post.auth.AuthorizationService;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+  private final AuthorizationService authorizationService;
+
   @GetMapping("/me")
-  public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal OAuth2User principal,
+  public ResponseEntity<?> getCurrentAdmin(@AuthenticationPrincipal OAuth2User principal,
       HttpServletRequest request) {
     if (principal == null) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
-    Cookie[] cookies = request.getCookies();
-    Arrays.stream(cookies).forEach(cookie -> {
-      System.out.println("쿠키 이름: " + cookie.getName() + ", 값: " + cookie.getValue());
-    });
     Map<String, Object> attributes = principal.getAttributes();
     Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
     Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
     String nickname = (String) profile.get("nickname");
+
+    authorizationService.validateAdminAccess(String.valueOf(attributes.get("id")));
+
     return ResponseEntity.ok(Map.of(
         "nickname", nickname,
         "isLoggedIn", true
     ));
-  }
-
-  @GetMapping("/csrf")
-  public CsrfToken csrf(CsrfToken token) {
-    return token; // JSON 형태로 token.value 내려줌
   }
 }
