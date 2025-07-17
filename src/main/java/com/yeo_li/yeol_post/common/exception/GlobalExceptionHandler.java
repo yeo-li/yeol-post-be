5848bef332446f0cd4b26a46b59124bad305194c
yeo-li <parkseongyeol2110@gmail.com>
2025-07-17 15:35:05 +0900

    refactor(GlobalExceptionHandler): 반환값 CommonResponse -> ResponseEntity 변경(#17)

--- src/main/java/com/yeo_li/yeol_post/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yeo_li/yeol_post/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.yeo_li.yeol_post.common.exception;
 
 import com.yeo_li.yeol_post.common.dto.CommonResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(BusinessException.class)
-  public CommonResponse<Void> handleBusinessException(BusinessException e) {
-    return CommonResponse.fail(e.getErrorCode());
+  public ResponseEntity<?> handleBusinessException(BusinessException e) {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(CommonResponse.fail(e.getErrorCode()));
   }
 
   @ExceptionHandler(Exception.class)
-  public CommonResponse<Void> handleOtherExceptions(Exception e) {
-    return CommonResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR);
+  public ResponseEntity<?> handleOtherExceptions(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(CommonResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR));
   }
 }
