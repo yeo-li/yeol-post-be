d69b4618871f389896c088edc885f294bd2f470d
yeo-li <parkseongyeol2110@gmail.com>
2025-07-17 15:41:34 +0900

    feat(postService): getPostByTitle 메서드 구현(#17)

--- src/main/java/com/yeo_li/yeol_post/post/PostService.java
+++ src/main/java/com/yeo_li/yeol_post/post/PostService.java
@@ -1,9 +1,11 @@
 package com.yeo_li.yeol_post.post;
 
 import com.yeo_li.yeol_post.post.command.PostCreateCommand;
+import com.yeo_li.yeol_post.post.dto.PostResponse;
 import com.yeo_li.yeol_post.post_tag.PostTagService;
 import com.yeo_li.yeol_post.tag.Tag;
 import com.yeo_li.yeol_post.tag.TagService;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,13 +20,49 @@ public class PostService {
   private final PostTagService postTagService;
 
   public int createPost(PostCreateCommand command) {
-    // 새로운 tag 저장
     List<Tag> tags = tagService.findOrCreateAll(command.tags());
-    // post 저장
     Post post = postRepository.save(command.toEntity());
-    // post_tag 저장
     postTagService.createPostTag(post, tags);
 
     return post.getId();
   }
+
+  public List<PostResponse> getPostByTitle(String title) {
+    if (title.isBlank()) {
+      return null;
+    }
+
+    List<Post> posts = postRepository.searchPostByTitle(title);
+    if (posts == null) {
+      return null;
+    }
+
+    posts.sort((p1, p2) -> p2.getPublishedAt().compareTo(p1.getPublishedAt()));
+    return convertPostResponse(posts);
+  }
+
+  public List<PostResponse> convertPostResponse(List<Post> posts) {
+    List<PostResponse> postResponses = new ArrayList<>();
+    for (Post post : posts) {
+      List<Tag> tags = postTagService.findTagByPostId(post.getId());
+      List<String> tagNames = new ArrayList<>();
+      for (Tag tag : tags) {
+        tagNames.add(tag.getTagName());
+      }
+
+      postResponses.add(new PostResponse(
+          post.getId(),
+          post.getTitle(),
+          post.getSummary(),
+          post.getAuthor(),
+          post.getContent(),
+          post.getPublishedAt(),
+          post.getCategory().getCategoryName(),
+          tagNames
+      ));
+    }
+
+    return postResponses;
+  }
+
 }
