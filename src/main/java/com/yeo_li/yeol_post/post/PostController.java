f63963146e8d9b162b733a31242862dde3788eaa
yeo-li <parkseongyeol2110@gmail.com>
2025-07-18 17:31:40 +0900

    feat(postController): api/v1/posts/{postId} DELETE api 추가(#15)

--- src/main/java/com/yeo_li/yeol_post/post/PostController.java
+++ src/main/java/com/yeo_li/yeol_post/post/PostController.java
@@ -13,7 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,6 @@ public class PostController {
   private final AuthorizationService authorizationService;
   private final PostService postService;
   private final PostCommandFactory postCommandFactory;
-  private final PostRepository postRepository;
 
   @PostMapping
   public ResponseEntity<?> savePost(@AuthenticationPrincipal OAuth2User principal,
@@ -60,6 +61,16 @@ public class PostController {
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(CommonResponse.success(postResponses));
+  }
+
+  @DeleteMapping("/{postId}")
+  public ResponseEntity<?> deletePost(@PathVariable("postId") int postId) {
+
+    postService.deletePostByPostId(postId);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(CommonResponse.success(null));
   }
 
 }
