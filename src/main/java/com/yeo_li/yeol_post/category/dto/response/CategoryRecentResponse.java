5028fde4d633426c1a7f01bd522a6a4e4bbf4e98
yeo-li <parkseongyeol2110@gmail.com>
2025-07-30 21:40:18 +0900

    feat(Category.dto): CategoryRecentResponse dto 작성(#53)

---
+++ src/main/java/com/yeo_li/yeol_post/category/dto/response/CategoryRecentResponse.java
@@ -0,0 +1,21 @@
+package com.yeo_li.yeol_post.category.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yeo_li.yeol_post.post.dto.PostResponse;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CategoryRecentResponse {
+
+  @JsonProperty("category_id")
+  private Long categoryId;
+  @JsonProperty("category_name")
+  private String categoryName;
+  private String description;
+  @JsonProperty("post_count")
+  private Integer postCount;
+  private PostResponse post;
+
+}
