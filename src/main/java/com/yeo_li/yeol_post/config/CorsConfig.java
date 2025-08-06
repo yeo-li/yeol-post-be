22fc5c1e8fb516dcc5c7b9ba984fcd9b0015a6b0
yeo-li <parkseongyeol2110@gmail.com>
2025-08-06 20:15:18 +0900

    fix: allowed method patch 추가

--- src/main/java/com/yeo_li/yeol_post/config/CorsConfig.java
+++ src/main/java/com/yeo_li/yeol_post/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins(frontendOrigin)
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                     .allowedHeaders("Content-Type")
                     .allowCredentials(true);
             }
